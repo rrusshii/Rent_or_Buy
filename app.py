70ffb8e22c799e0ff766b17bae2998f2b62d62fd
rrusshii <rushikeshsherekar@gmail.com>
2025-04-24 18:30:03 +0530

    removng unsued imports

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import plotly.express as px
-import plotly.graph_objects as go
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
